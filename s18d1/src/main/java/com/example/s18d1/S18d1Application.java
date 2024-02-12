0be77d41b275606d4914b48af5a4c382add3aa40
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2024-02-12 15:31:42 +0300

    project created

---
+++ s18d1/src/main/java/com/example/s18d1/S18d1Application.java
@@ -0,0 +1,13 @@
+package com.example.s18d1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S18d1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S18d1Application.class, args);
+	}
+
+}
---
+++ s18d1/src/test/java/com/example/s18d1/S18d1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s18d1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S18d1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
